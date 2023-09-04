bec5fbddc6d66c0e86ee36af8424756759786362
Areshkevich_D <DAreshkevich@iba.by>
2023-09-04 12:30:14 +0300

    test refactoring

--- internal/app/handler_test/handler_test.go
+++ internal/app/handler_test/handler_test.go
@@ -1,65 +1,29 @@
 package handler
 
 import (
-	"github.com/denis-oreshkevich/shortener/internal/app/constant"
+	"github.com/denis-oreshkevich/shortener/internal/app/handler"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"io"
 	"net/http"
 	"net/http/httptest"
-	"regexp"
 	"strings"
 	"testing"
 )
 
-var regex = regexp.MustCompile(constant.ServerURL + "/[A-Za-z]{8}")
-
-type want struct {
-	contentType    string
-	statusCode     int
-	body           string
-	headerLocation string
-}
-type Req struct {
-	url    string
-	method string
-	body   string
-}
-type test struct {
-	name   string
-	isMock bool
-	mockOn func(m *mockedRepository) *mock.Call
-	req    Req
-	want   want
-}
-
-type mockedRepository struct {
-	mock.Mock
-}
-
-func (m *mockedRepository) SaveURL(id, url string) {
-	m.Called(id, url)
-}
-
-func (m *mockedRepository) FindURL(id string) (string, bool) {
-	args := m.Called(id)
-	return args.String(0), args.Bool(1)
-}
-
 func TestHandlePost(t *testing.T) {
-	postTests := []test{
+	postTests := []Test{
 		{
 			name:   "simple post test #1",
 			isMock: true,
-			mockOn: func(m *mockedRepository) *mock.Call {
+			mockOn: func(m *MockedRepository) *mock.Call {
 				return m.On("SaveURL", mock.Anything, mock.Anything).Return()
 			},
-			req: Req{
-				url:    "/",
-				method: "POST",
-				body:   "https://practicum.yandex.ru/",
+			reqFunc: func() *http.Request {
+				body := strings.NewReader("https://practicum.yandex.ru/")
+				return httptest.NewRequest("POST", "/", body)
 			},
-			want: want{
+			want: Want{
 				contentType: "text/plain",
 				statusCode:  201,
 			},
@@ -67,12 +31,11 @@ func TestHandlePost(t *testing.T) {
 		{
 			name:   "bad url post test #2",
 			isMock: false,
-			req: Req{
-				url:    "/test",
-				method: "POST",
-				body:   "https://practicum.yandex.ru/",
+			reqFunc: func() *http.Request {
+				body := strings.NewReader("https://practicum.yandex.ru/")
+				return httptest.NewRequest("POST", "/test", body)
 			},
-			want: want{
+			want: Want{
 				contentType: "text/plain",
 				statusCode:  400,
 			},
@@ -80,12 +43,11 @@ func TestHandlePost(t *testing.T) {
 		{
 			name:   "bad body url post test #3",
 			isMock: false,
-			req: Req{
-				url:    "/",
-				method: "POST",
-				body:   "ahahahah",
+			reqFunc: func() *http.Request {
+				body := strings.NewReader("ahahahah")
+				return httptest.NewRequest("POST", "/", body)
 			},
-			want: want{
+			want: Want{
 				contentType: "text/plain",
 				statusCode:  400,
 			},
@@ -95,18 +57,17 @@ func TestHandlePost(t *testing.T) {
 }
 
 func TestHandleGet(t *testing.T) {
-	getTests := []test{
+	getTests := []Test{
 		{
 			name:   "simple get test #1",
 			isMock: true,
-			mockOn: func(m *mockedRepository) *mock.Call {
+			mockOn: func(m *MockedRepository) *mock.Call {
 				return m.On("FindURL", "/AAAAAAAA").Return("http://localhost:30001/", true)
 			},
-			req: Req{
-				url:    "/AAAAAAAA",
-				method: "GET",
+			reqFunc: func() *http.Request {
+				return httptest.NewRequest("GET", "/AAAAAAAA", nil)
 			},
-			want: want{
+			want: Want{
 				contentType:    "text/plain",
 				statusCode:     307,
 				headerLocation: "http://localhost:30001/",
@@ -115,12 +76,10 @@ func TestHandleGet(t *testing.T) {
 		{
 			name:   "bad url get test #2",
 			isMock: false,
-			req: Req{
-				url:    "/test/AAAAAAAA",
-				method: "GET",
-				body:   "https://practicum.yandex.ru/",
+			reqFunc: func() *http.Request {
+				return httptest.NewRequest("GET", "/test/AAAAAAAA", nil)
 			},
-			want: want{
+			want: Want{
 				contentType: "text/plain",
 				statusCode:  400,
 			},
@@ -128,14 +87,13 @@ func TestHandleGet(t *testing.T) {
 		{
 			name:   "not stored url get test #3",
 			isMock: true,
-			mockOn: func(m *mockedRepository) *mock.Call {
+			mockOn: func(m *MockedRepository) *mock.Call {
 				return m.On("FindURL", "/HHHHHHHH").Return("", false)
 			},
-			req: Req{
-				url:    "/HHHHHHHH",
-				method: "GET",
+			reqFunc: func() *http.Request {
+				return httptest.NewRequest("GET", "/HHHHHHHH", nil)
 			},
-			want: want{
+			want: Want{
 				contentType: "text/plain",
 				statusCode:  400,
 			},
@@ -144,18 +102,11 @@ func TestHandleGet(t *testing.T) {
 	runSubTests(t, getTests)
 }
 
-func runSubTests(t *testing.T, tests []test) {
-	testObj := new(mockedRepository)
+func runSubTests(t *testing.T, tests []Test) {
+	testObj := new(MockedRepository)
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			var request *http.Request
-			if tt.req.body != "" {
-				body := strings.NewReader(tt.req.body)
-				request = httptest.NewRequest(tt.req.method, tt.req.url, body)
-			} else {
-				request = httptest.NewRequest(tt.req.method, tt.req.url, nil)
-
-			}
+			request := tt.reqFunc()
 			w := httptest.NewRecorder()
 
 			var mockCall *mock.Call
@@ -164,7 +115,7 @@ func runSubTests(t *testing.T, tests []test) {
 				mockCall = tt.mockOn(testObj)
 			}
 
-			handlerFunc := URL(testObj)
+			handlerFunc := handler.URL(testObj)
 			handlerFunc.ServeHTTP(w, request)
 
 			if tt.isMock {
@@ -181,7 +132,7 @@ func runSubTests(t *testing.T, tests []test) {
 				defer result.Body.Close()
 				respBody, err := io.ReadAll(result.Body)
 				assert.NoError(t, err)
-				assert.True(t, regex.MatchString(string(respBody)))
+				assert.True(t, IdRegex.MatchString(string(respBody)))
 			}
 		})
 	}
