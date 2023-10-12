24ba3e0e9a99bad50ee9787a544c0c1a66637888
Areshkevich_D <DAreshkevich@iba.by>
2023-10-13 02:23:39 +0300

    log ids

--- cmd/shortener/common_test.go
+++ cmd/shortener/common_test.go
@@ -14,7 +14,7 @@ import (
 	"testing"
 )
 
-var IDURLRegex = regexp.MustCompile(config.Get().BaseURL() + "/[A-Za-z0-9]$")
+var IDURLRegex = regexp.MustCompile(config.Get().BaseURL() + "/[A-Za-z0-9]{8}$")
 
 type mockedStorage struct {
 	mock.Mock
