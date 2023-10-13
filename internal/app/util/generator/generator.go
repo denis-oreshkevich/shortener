ae1443e9316deb09c8f4b549cbd4d9b5e78e42e7
Areshkevich_D <DAreshkevich@iba.by>
2023-10-13 11:54:36 +0300

    try noinline

--- internal/app/util/generator/generator.go
+++ internal/app/util/generator/generator.go
@@ -3,14 +3,16 @@ package generator
 import (
 	"github.com/denis-oreshkevich/shortener/internal/app/util/logger"
 	"math/rand"
+	"time"
 )
 
-//func init() {
-//	rand.NewSource(time.Now().UnixNano())
-//}
+func init() {
+	rand.NewSource(time.Now().UnixNano())
+}
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
 
+//go:noinline
 func RandString(n int) string {
 	b := make([]rune, n)
 	for i := range b {
