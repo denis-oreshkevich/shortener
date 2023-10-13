8c13895dbe289fd74e0efe14922f9e4f70a91d3a
Areshkevich_D <DAreshkevich@iba.by>
2023-10-13 13:13:55 +0300

    generator fix

--- internal/app/util/generator/generator.go
+++ internal/app/util/generator/generator.go
@@ -1,23 +1,16 @@
 package generator
 
 import (
-	"github.com/denis-oreshkevich/shortener/internal/app/util/logger"
 	"math/rand"
 	"time"
 )
 
-func init() {
-	rand.NewSource(time.Now().UnixNano())
-}
-
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-
-//go:noinline
-func RandString(n int) string {
-	b := make([]rune, n)
-	for i := range b {
-		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+func RandString(length int) string {
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	characters := `ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`
+	result := make([]byte, length)
+	for i := 0; i < length; i++ {
+		result[i] = characters[rand.Intn(len(characters))]
 	}
-	logger.Log.Info("generated ID is " + string(b))
-	return string(b)
+	return string(result)
 }
