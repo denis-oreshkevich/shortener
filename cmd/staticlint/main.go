8c57ec39933e3e92ecae0833cb3e4e7d0f0b4e71
Areshkevich_D <DAreshkevich@iba.by>
2024-01-22 15:26:32 +0300

    add test and move to pkg

--- cmd/staticlint/main.go
+++ cmd/staticlint/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/denis-oreshkevich/shortener/cmd/staticlint/osexit"
+	"github.com/denis-oreshkevich/shortener/pkg/osexit"
 	"golang.org/x/tools/go/analysis"
 	"golang.org/x/tools/go/analysis/multichecker"
 	"golang.org/x/tools/go/analysis/passes/defers"
